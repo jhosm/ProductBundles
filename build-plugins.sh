64dc1c2e87a8dc8b7b4f3ed2ce4fb0b6cb747a24
jhosm <joao.hugo.miranda@gmail.com>
2025-07-19 23:07:25 +0100

    Fixed the build_plugins script so that it copies the SamplePlugin to the correct locations

--- build-plugins.sh
+++ build-plugins.sh
@@ -9,10 +9,17 @@ mkdir -p plugins
 echo "Building ProductBundles.SamplePlugin..."
 dotnet build ProductBundles.SamplePlugin/ProductBundles.SamplePlugin.csproj -o plugins/SamplePlugin
 
-# Copy the plugin DLL to the plugins folder
+# Create necessary directories
+echo "Creating plugin directories..."
+mkdir -p ProductBundles.Api/plugins
+mkdir -p ProductBundles.PluginLoader/bin/Debug/net8.0/plugins
+
+# Copy the plugin DLL to both ProductBundles.Api and ProductBundles.PluginLoader
+echo "Copying plugins to ProductBundles.Api..."
+cp plugins/SamplePlugin/ProductBundles.SamplePlugin.dll ProductBundles.Api/plugins/
+
+echo "Copying plugins to ProductBundles.PluginLoader..."
 cp plugins/SamplePlugin/ProductBundles.SamplePlugin.dll ProductBundles.PluginLoader/bin/Debug/net8.0/plugins/
-cp plugins/SamplePlugin/ProductBundles.Core.dll ProductBundles.PluginLoader/bin/Debug/net8.0/plugins/
-cp plugins/SamplePlugin/ProductBundles.Sdk.dll ProductBundles.PluginLoader/bin/Debug/net8.0/plugins/
 
 # Build the main application
 echo "Building ProductBundles.PluginLoader..."
